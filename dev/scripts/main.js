5f6e49672b7174c4332613873e9093e0b64e391f
Jackie Evershed <jackie.evershed@gmail.com>
2016-08-11 10:58:41 -0400

    added values and images

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -7,40 +7,52 @@ events.version = '20130815';
 
 var scenarios = [
 	{
-		instance: "Transport yourself to the center of helm's deep during the Battle of Hornburg to throw a battle axe into the skull of an Uruk Hai."
+		instance: "Transport yourself to the center of helm's deep during the Battle of Hornburg to throw a battle axe into the skull of an Uruk Hai.",
+		value: "BATL"
 	},
 	{
-		instance: "Stroll down Park Place with Rich Uncle Penny Bags making it rain because you know you’re about to pass GO."
+		instance: "Stroll down Park Place with Rich Uncle Penny Bags making it rain because you know you’re about to pass GO.",
+		value: "castle board game cafe"
 	},
 	{
-		instance: "Kill some time during the Punic Wars against Carthage by throwing rocks at some other rocks with your fellow soldiers."
+		instance: "Kill some time during the Punic Wars against Carthage by throwing rocks at some other rocks with your fellow soldiers.",
+		value: "Track & Field"
 	},
 	{
-		instance: "Spank some balls with a wooden paddle while sipping on a Negroni with all your pals."
+		instance: "Spank some balls with a wooden paddle while sipping on a Negroni with all your pals.",
+		value: "spin toronto"
 	},
 	{
-		instance: "Cease to be conscious of ones self as the one who is engaged in hitting a bulls-eye that confronts you."
+		instance: "Cease to be conscious of ones self as the one who is engaged in hitting a bulls-eye that confronts you.",
+		value: "archery district"
 	},
 	{
-		instance: "Break into a museum with an elite team of thieves tasked with acquiring an illusive and mysterious relic."
+		instance: "Break into a museum with an elite team of thieves tasked with acquiring an illusive and mysterious relic.",
+		value: "escape games"
 	},
 	{
-		instance: "Travel back in time to be a contestant on American Gladiators only to have your dreams of winning shattered by some muscle bound behemoth named Bronco."
+		instance: "Travel back in time to be a contestant on American Gladiators only to have your dreams of winning shattered by some muscle bound behemoth named Bronco.",
+		value: "Pursuit OCR"
 	},
 	{
-		instance: "Dangle off the edge of Yellow Mountain while you watch your safety line slowly drift 900 meters to the ground."
+		instance: "Dangle off the edge of Yellow Mountain while you watch your safety line slowly drift 900 meters to the ground.",
+		value: "Boulderz"
 	},
 	{
-		instance: "Spend your free time in the ice age bouncing on the stretched skin of a wooly mammoth."
+		instance: "Spend your free time in the ice age bouncing on the stretched skin of a wooly mammoth.",
+		value: "Sky Zone"
 	},
 	{
-		instance: "Sip a tasty non-fat-one-pump-no-whip mocha while being smothered by a barrage of kitties."
+		instance: "Sip a tasty non-fat-one-pump-no-whip mocha while being smothered by a barrage of kitties.",
+		value: "TOT cat cafe"
 	},
 	{
-		instance: "Exist inside a gelatinous ectoplasm playing for Manchester United in the FA Cup Final."
+		instance: "Exist inside a gelatinous ectoplasm playing for Manchester United in the FA Cup Final.",
+		value: "Bubble Soccer Toronto"
 	},
 	{
-		instance: "Run through a post apocalyptic battle field equipped with a photon blaster."
+		instance: "Run through a post apocalyptic battle field equipped with a photon blaster.",
+		value: "Laser Quest"
 	},
 ];
 //Pull Two Scenarios At Random Out Of Object
