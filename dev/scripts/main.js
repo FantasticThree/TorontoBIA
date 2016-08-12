0a99fd1aa0b823f21cade343738f44676dd9523b
Noah Endale <noah.endale@gmail.com>
2016-08-12 12:54:38 -0400

    added wikipedia and reset links

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -1,4 +1,5 @@
 var events = {};
+var wiki = {};
 events.clientID = 'LSYF2M1LMY4HXFFC3FAXQCP4DCWHRHUCIT2UTCFI03RF0ZG1';
 events.clientSecret = 'MUHQ2Q5JOZDSVKIZWEQANDK2QA4PHCMCAEHW1QNBEIFHZFTK';
 events.version = '20130815';
@@ -9,62 +10,74 @@ var scenarios = [
 	{
 		instance: "Transport yourself to the center of helm's deep during the Battle of Hornburg to throw a battle axe into the skull of an Uruk Hai.",
 		searchValue: "BATL",
-		image: "public/styles/images/BATL.jpg"
+		image: "public/styles/images/BATL.jpg",
+		activity: 'axe throwing'
 	},
 	{
 		instance: "Stroll down Park Place with Rich Uncle Penny Bags making it rain because you know you’re about to pass GO.",
 		searchValue: "castle board game cafe",
-		image: "public/styles/images/Castle.jpg"
+		image: "public/styles/images/Castle.jpg",
+		activity: 'board game' //HOW TO SCROLL TO HISTORY SECTION AUTOMATICALLY????
 	},
 	{
 		instance: "Kill some time during the Punic Wars against Carthage by throwing rocks at some other rocks with your fellow soldiers.",
 		searchValue: "Track & Field",
-		image: "public/styles/images/trackAndField.jpg"
+		image: "public/styles/images/trackAndField.jpg",
+		activity: 'bocce'
 	},
 	{
 		instance: "Spank some balls with a wooden paddle while sipping on a Negroni with all your pals.",
 		searchValue: "spin toronto",
-		image: "public/styles/images/Spin.jpg"
+		image: "public/styles/images/Spin.jpg",
+		activity: 'ping pong'
 	},
 	{
 		instance: "Cease to be conscious of ones self as the one who is engaged in hitting a bulls-eye that confronts you.",
 		searchValue: "archery district",
-		image: "public/styles/images/ArcheryDistrict.jpg"
+		image: "public/styles/images/ArcheryDistrict.jpg",
+		activity: 'history of archery'
 	},
 	{
 		instance: "Break into a museum with an elite team of thieves tasked with acquiring an illusive and mysterious relic.",
 		searchValue: "escape games",
-		image: "public/styles/images/"
+		image: "public/styles/images/",
+		activity: 'escape room'
 	},
 	{
 		instance: "Travel back in time to be a contestant on American Gladiators only to have your dreams of winning shattered by some muscle bound behemoth named Bronco.",
 		searchValue: "Pursuit OCR",
-		image: "public/styles/images/pursuitOCR.jpg"
+		image: "public/styles/images/pursuitOCR.jpg",
+		activity: 'obstacle racing'
 	},
 	{
 		instance: "Dangle off the edge of Yellow Mountain while you watch your safety line slowly drift 900 meters to the ground.",
 		searchValue: "Boulderz",
-		image: "public/styles/images/Boulderz.jpg"
+		image: "public/styles/images/Boulderz.jpg",
+		activity: 'bouldering'
 	},
 	{
 		instance: "Spend your free time in the ice age bouncing on the stretched skin of a wooly mammoth.",
 		searchValue: "Sky Zone",
-		image: "public/styles/images/SkyZone.jpg"
+		image: "public/styles/images/SkyZone.jpg",
+		activity: 'trampoline'
 	},
 	{
 		instance: "Sip a tasty non-fat-one-pump-no-whip mocha while being smothered by a barrage of kitties.",
 		searchValue: "TOT cat cafe",
-		image: "public/styles/images/CatCafe.jpg"
+		image: "public/styles/images/CatCafe.jpg",
+		activity: 'cat cafe'
 	},
 	{
 		instance: "Exist inside a gelatinous ectoplasm playing for Manchester United in the FA Cup Final.",
 		searchValue: "Bubble Soccer Toronto",
-		image: "public/styles/images/BubbleSoccer.jpg"
+		image: "public/styles/images/BubbleSoccer.jpg",
+		activity: 'bubble bump football'
 	},
 	{
 		instance: "Run through a post apocalyptic battle field equipped with a photon blaster.",
 		searchValue: "Laser Quest",
-		image: "public/styles/images/LaserQuest.jpg"
+		image: "public/styles/images/LaserQuest.jpg",
+		activity: 'laser tag'
 	},
 ];
 //Pull Two Scenarios At Random Out Of Object
@@ -95,8 +108,9 @@ function init() {
 		var userChoice = $("input[type=radio]:checked").val();
 		events.getInfo(scenarios[userChoice].searchValue);
 		events.eventNumber = userChoice;
+		wiki.getInfo(scenarios[userChoice].activity);
 	});
-}
+};
 
 events.getInfo = function(userChoice) {
 	$.ajax({
@@ -116,6 +130,37 @@ events.getInfo = function(userChoice) {
 		// console.log(data.response.venues[0]);
 	});
 };
+
+wiki.getInfo = function(query) {
+	$.ajax({
+		url: 'https://en.wikipedia.org/w/api.php',
+		method: 'GET',
+		dataType: 'jsonp',
+		rvprop: 'content',
+		data: {
+			action: 'query',
+			titles: query,
+			format: 'json',
+			prop: 'info',
+			inprop: 'url'
+			// prop: 'revision',
+			// rvlimit: 1,
+			// rvprop: 'content'
+			// rvsection: 0
+		}
+	}).then(function(response) {
+		for (var key in response.query.pages){
+			wiki.displayInfo(response.query.pages[key].canonicalurl);
+		};
+		console.log(response);
+	});
+};
+
+wiki.displayInfo = function(wikiLink){
+	$('.wikiContainer').empty();
+		var activityLink = $('<a>').attr('href', wikiLink + '#History').text("History").attr('target', '_blank');
+		$('.wikiContainer').append(activityLink);
+}
 
 events.displayInfo = function(venue, userNumber){
 //GRAB NAME, URL, PHONE#, ADDRESS, STORE HOURS FROM FIRST RESULT
